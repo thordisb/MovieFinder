de9ab327199fd88df34af67095779eab45dc18c0
Þórdís Björk Þórisdóttir <thordis@ordiss-MacBook-Air.local>
2017-02-14 14:42:11 +0000

    skárri slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function(){
 	$('superslider').slick({
 		infinite: true,
@@ -54,18 +54,45 @@ r.onreadystatechange = function () {
 };
 r.send("banana=yellow");
 });
-=======
+
+/*----------------------------------------slickslider------------------------*/
+
  $(document).on('ready', function() {
-      $(".center").slick({
-        dots: true,
-        infinite: true,
+    $('.responsive').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
         slidesToShow: 3,
-        slidesToScroll: 3
-<<<<<<< HEAD
-      })
-       });
-=======
+        slidesToScroll: 3,
+        infinite: true,
+        
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
       });
-       });
->>>>>>> e6f0877f545d0685d5efb569fb6b76ef0094913c
->>>>>>> 600ff2986cfb5a245be374f87bc2aea1bfcce2b7
+       
+
